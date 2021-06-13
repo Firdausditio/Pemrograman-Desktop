6e886d1889ea6dfe76b3255e0ada76e1da5a0767
Firdausditio <71175823+Firdausditio@users.noreply.github.com>
2021-06-13 12:20:17 +0700

    Add files via upload

---
+++ K3519034_proyek49/K3519034_proyek49/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If (TextBox_pass.Text = "12345") Then
+            Form2.Show()
+        Else
+            MessageBox.Show("Password Salah")
+
+        End If
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+End Class
