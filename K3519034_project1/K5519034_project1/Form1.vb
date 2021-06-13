f9de3efb4efc3debc6250c74485888d968f6b8b1
Firdausditio <71175823+Firdausditio@users.noreply.github.com>
2021-06-13 12:19:29 +0700

    Add files via upload

---
+++ K3519034_project1/K5519034_project1/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Hai Iam Firdaus Ditio Ramadhan K3519034 from Indonesia")
+
+    End Sub
+End Class
