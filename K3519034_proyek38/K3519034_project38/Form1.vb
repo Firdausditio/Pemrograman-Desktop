17d16fd6f8a9b711ebdd3657f3a10cd071e171fb
Firdausditio <71175823+Firdausditio@users.noreply.github.com>
2021-06-13 12:34:16 +0700

    Add files via upload

---
+++ K3519034_proyek38/K3519034_project38/Form1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        My.Computer.Network.DownloadFile("https://mdr-corporation.com/assets/img/about.jpeg", "D:/gambar.jpeg")
+    End Sub
+End Class
---
+++ K3519034_proyek39/K3519034_proyek39/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim Myconnection As System.Data.OleDb.OleDbConnection
+            Dim dataset As System.Data.DataSet
+            Dim MyCommand As System.Data.OleDb.OleDbDataAdapter
+            Dim path As String = "E:\\[TUGAS UNS]\\Semester 4\\Pem Desk"
+
+            Myconnection = New System.Data.OleDb.OleDbConnection("Provide=Microsoft.ACE.OLEDB.12.0;Data Source=" + path + ";Extended properties=Excel 12.0;")
+            MyCommand = New System.Data.OleDb.OleDbDataAdapter("select = from [Sheet1$]", Myconnection)
+
+            dataset = New System.Data.DataSet
+            MyCommand.Fill(dataset)
+            DataGridView1.DataSource = dataset.Tables(0)
+
+            Myconnection.Close()
+
+        Catch ex As Exception
+            MsgBox(ex.Message.ToString)
+        End Try
+    End Sub
+End Class
